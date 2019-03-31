fc5a063fffc6d32fa40f5173e3d702afd6c0104a
Mikkel Kroman <mk@uplink.io>
2019-03-31 05:23:34 +0200

    Save the config path and refactor get_streams

--- src/twitch_recorder.py
+++ src/twitch_recorder.py
@@ -14,46 +14,46 @@ class TwitchRecorder:
     # Constructs a new TwitchRecorder instance.
     def __init__(self):
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.streaming_processes = []
 
     # Loads the TOML file at the given +path+ and uses it as a config file.
     def load_config(self, path):
         config = toml.load(path)
 
         self.config = config
-        self.twitch_api_key = config['twitch'].get(
-                'key', os.environ.get('TWITCH_API_KEY'))
-        self.twitch_client = TwitchClient(self.twitch_api_key)
+        self.config_path = path
         self.check_interval = config['twitch'].get(
                 'check_interval', self.DEFAULT_CHECK_INTERVAL)
+        twitch_api_key = config['twitch'].get(
+                'key', os.environ.get('TWITCH_API_KEY'))
+        self.twitch_client = TwitchClient(twitch_api_key)
 
     def run(self):
-        self.logger.debug('running')
+        streamers = self.config['streamers']
+        self.logger.debug('{} streamers loaded: {}'.format(
+            len(streamers),
+            ', '.join(streamers)
+            ))
 
         while True:
             self.poll()
 
-            self.logger.debug('Checking again in {}s'.format(self.check_interval))
+            self.logger.debug('Checking again in {}s'.format(
+                self.check_interval))
             time.sleep(self.check_interval)
-
-    def get_streams(self, streamer_names):
-        params = {'user_login': streamer_names, 'first': 100}
-
-        res = self.twitch_client.get_streams(params)
-        streams = res.get('data', [])
-
-        return streams
 
     # Polls the streamer statuses from the Twitch API.
     def poll(self):
         streamers = self.config['streamers']
-        result = self.get_streams(streamers.keys())
+        res = self.twitch_client.get_streams({'user_login': streamers.keys(),
+                                              'first': 100})
+        streams = res.get('data', [])
 
-        for stream in result:
+        for stream in streams:
             username = stream['user_name'].lower()
             options = streamers[username]
 
-            self.logger.debug("{} is live - options: {}".format(username, options))
+            self.logger.debug("{} is live - options: {}".format(
+                username, options))
 
 
 if __name__ == '__main__':
